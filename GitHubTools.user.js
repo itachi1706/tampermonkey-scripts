5eee463cc01f06d40f5f273e820a8fdcf996aabb
Kenneth Soh <kennethsohyq@gmail.com>
2020-06-24 17:27:31 +0800

    Tweaked image for new GitHub interface

--- GitHubTools.user.js
+++ GitHubTools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Tools
 // @namespace    http://itachi1706.com/
-// @version      1.6.2
+// @version      1.6.3
 // @description  Small tweaks to GitHub for QoL improvements
 // @author       Kenneth Soh (itachi1706) <kenneth@itachi1706.com>
 // @updateURL    https://github.com/itachi1706/tampermonkey-scripts/raw/master/GitHubTools.user.js
@@ -45,7 +45,7 @@
             console.log("Detected search screen. Exiting");
             return;
         }
-        var navBar = $(".reponav ul");
+        var navBar = $(".js-repo-nav ul");
         if (debug) console.log(navBar);
         if (debug) console.log(paths);
         // Trying to find insight to insert after. Otherwise insert before settings, otherwise insert at end
@@ -67,16 +67,15 @@
         });
 
         var hrefStr = '/' + paths[1] + '/' + paths[2] + '/compare';
-        var compareImg = '<li><a id="octo-compare" class="js-selected-navigation-item reponav-item" href="' + hrefStr + '">' +
-            '<svg class="octicon octicon-git-compare" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" ' +
+        var compareImg = '<li class="d-flex"><a id="octo-compare" class="js-selected-navigation-item UnderlineNav-item hx_underlinenav-item no-wrap js-responsive-underlinenav-item" href="' + hrefStr + '">' +
+            '<svg class="octicon octicon-git-compare UnderlineNav-octicon d-none d-sm-inline" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" ' +
             'd="M5 12H4c-.27-.02-.48-.11-.69-.31-.21-.2-.3-.42-.31-.69V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V11c.03.78.34 1.47.94 2.06.6.59 1.28.91 2.06.94h1v2l3-3-3-3v2zM2 1.8c.66 0 1.2.55 1.2 ' +
             '1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm11 9.48V5c-.03-.78-.34-1.47-.94-2.06-.6-.59-1.28-.91-2.06-.94H9V0L6 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 ' +
-            '0 0 0 12 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>      Compare</a></li>';
+            '0 0 0 12 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg><span data-content="Compare">Compare</span></a></li>';
         console.log("Injecting Compare Link");
         if (!iBefore) $(iInto).after(compareImg);
         else $(iInto).before(compareImg);
         checkIfCompareStillExists();
-        $('.container-lg').css('max-width', '1040px');
 
         // Check if on release page
         if (paths.length >= 4 && paths[3] == 'releases') {
