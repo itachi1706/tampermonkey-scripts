c97ef3e620db00de346f8dd8b002914ed303dbe5
Kenneth Soh <kennethsohyq@gmail.com>
2020-08-04 21:09:06 +0800

    Improved the look and match Colab own button
    This make it seems more integrated into the web application instead of looking extremely out of place

--- ColabReconnector.user.js
+++ ColabReconnector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Colab Auto-Reconnect
 // @namespace    http://itachi1706.com/
-// @version      0.1
+// @version      1.0
 // @description  Adds a basic button to toggle auto reconnection in Google Colab
 // @updateURL    https://github.com/itachi1706/tampermonkey-scripts/raw/master/ColabReconnector.user.js
 // @author       Kenneth Soh (itachi1706) <kenneth@itachi1706.com>
@@ -12,15 +12,35 @@
 (function() {
     'use strict';
 
+    // Localization
+    const txtEnable = "Enable Auto-Reconnect";
+    const txtEnabling = "Enabled!";
+    const txtDisable = "Disable Auto-Reconnect";
+    const txtDisabling = "Disabled!";
+    const txtEnableTooltip = "Enables a 15 seconds automatic reconnector";
+    const txtDisableTooltip = "Disables automatic reconnector";
+    // End of localization
+
     let id = -1;
 
-    let btn = document.createElement('button');
-    btn.type = "button";
-    btn.innerText = "Enable Auto-Reconnect";
+    let btn = document.createElement('paper-button');
+    let icon = document.createElement('iron-icon');
+    let btnspan = document.createElement('span');
+    icon.icon = "refresh";
+    btnspan.innerText = txtEnable;
+    btn.setAttribute("command", "noop");
+    btn.id = "autoreload";
     btn.onclick = function() {
         if (id === -1) startConnect();
         else clearConnect();
     }
+
+    let tooltip = document.createElement('paper-tooltip');
+    let tooltiptext = document.createElement('div');
+    tooltip.setAttribute("for", "autoreload");
+    tooltiptext.className = "tooltip-with-shortcut";
+    tooltiptext.innerText = txtEnableTooltip;
+    tooltip.id = "autoreload-tooltip";
 
     function clickConnect() {
         console.log("Clicked on connect button");
@@ -30,15 +50,19 @@
 
     function startConnect() {
         console.log("Starting connect");
-        btn.innerText = "Enabling Auto-Reconnector every 15 seconds...";
+        btnspan.innerText = txtEnabling;
+        tooltiptext.innerText = txtDisableTooltip;
+        icon.icon = "check"
         id = setInterval(clickConnect, 15000);
-        setTimeout(function() { btn.innerText = "Disable Auto-Reconnect"; }, 2000);
+        setTimeout(function() { btnspan.innerText = txtDisable; icon.icon = "refresh"; }, 500);
     }
 
     function clearConnect() {
         console.log("Clearing connect");
-        btn.innerText = "Disabling Auto-Reconnector...";
-        setTimeout(function() { btn.innerText = "Enable Auto-Reconnect"; }, 2000);
+        btnspan.innerText = txtDisabling;
+        tooltiptext.innerText = txtEnableTooltip;
+        icon.icon = "clear";
+        setTimeout(function() { btnspan.innerText = txtEnable; icon.icon = "refresh" }, 500);
         if (id === -1) return;
         clearInterval(id);
         id = -1;
@@ -52,7 +76,12 @@
             return;
         }
         console.log("Initialized. Adding reconnect button");
-        header.insertBefore(btn, header.firstChild);
+        let child = header.firstChild;
+        header.insertBefore(btn, child);
+        btn.appendChild(icon);
+        btn.appendChild(btnspan);
+        header.insertBefore(tooltip, child);
+        tooltip.appendChild(tooltiptext);
     }
 
     window.onload = function() {
